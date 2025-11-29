6c59228a8adfdf6c6cfafc2a52e39ee44caf4462
mohamedaarif1811@gmail.com <mohamedaarif1811@gmail.com>
2025-11-29 14:47:27 +0530

    Error fix

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["*"]
 )
 
